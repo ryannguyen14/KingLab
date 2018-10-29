818b1d8b143352e4eb585e081f312e4bb580965e
Ryan Nguyen <ryan.nguyen@yale.edu>
2018-10-28 20:28:15 -0400

    Ain't nothing but a mistake

--- swi6_dynam.py
+++ swi6_dynam.py
@@ -33,14 +33,10 @@ class Particle(sphere):
 class Fuse(compound):
 
 
-	def giveRadius(self,particle):
+	def giveProperties(self, particle, num, mass):
 		self.radius = particle.radius
 		self.hasOligomerized = False
-
-	def giveNum(self,num):
 		self.num = num
-	
-	def giveMass(self,mass):
 		self.mass = mass
 	
 	def calculateEnergy(self, particle_array):
@@ -62,6 +58,48 @@ class Fuse(compound):
 		return force_sum 
 
 
+	"""" Check for Contacts before Doing Anything Else""" 
+	def oligoCheck(self,particle_array):
+	""" Find out if dimerization should occur """
+		
+	""" Get Distances """
+	for particle_index in range(0,len(particle_array)):
+		if particle_index != self.num:
+			distance_x, distance_y, distance_z, distance_r = self.calculateDistance(particle_array[particle_index])
+			distance_array_x.append(distance_x)
+			distance_array_y.append(distance_y)
+			distance_array_z.append(distance_z)
+			distance_array_r.append(distance_r)
+			particle_num_array.append(particle_array[particle_index].num)
+
+	oligomer_pair_list = []
+
+	# This gets all of the particls that are close enough to oligomerize. Double counting does happen!!! 
+	for j in range(0, len(distance_array_r)):
+		if distance_array_r[j] <= sigma and particle_num_array[j] != self.num: 
+			oligomer_pair_list = oligomer_pair_list.append((self.num,particle_num_array[j]))
+
+
+
+	doubleCount = False 
+	for pair_idx in 0,oligomer_pair_list:
+		oligomer_pair_list.pop((oligomer_pair_list[pair_idx][1], oligomer_pair_list[pair_idx][0]))
+		# To account for double counting, only count pair (a,b), not (b,a)
+		if (oligomer_pair_list[pair_idx][1], oligomer_pair_list[pair_idx][0]) in oligomer_pair_list:
+			doubleCount = True
+
+		if not doubleCount: 
+			temp_oligomer = Oligomer([particle_array[pair[0]],particle_array[pair[1]]])
+			oligomer_pair_list[pair_idx][0].visible = False
+			oligomer_pair_list[pair_idx][1].visible = False
+
+
+		# temp_oligomer.giveMass(particle_array[pair[0]], particle_array[pair[1]])
+		# temp_oligomer.giveRadius(particle_array[pair[0]], particle_array[pair[1]])
+		# temp_oligomer.giveNum(len(particle_array))
+		# particle_array.append(temp_oligomer)
+	
+
 	def update(self, particle_array = [], delta_t = 1): 
 
 
@@ -69,40 +107,15 @@ class Fuse(compound):
 		#Variance is set to the mean squared displacement 
 		delta_x, delta_y, delta_z = np.random.normal(0, 2*D_trans*delta_t, 3)
 
+
+
+
 		force_sum_x, force_sum_y, force_sum_z, force_total = self.calculateEnergy(particle_array)
 
 		self.pos.x = self.pos.x + delta_x + (D_trans/kT *force_sum_x*delta_t)
 		self.pos.y = self.pos.y + delta_y + (D_trans/kT *force_sum_y*delta_t)
 		self.pos.z = self.pos.z + delta_z + (D_trans/kT *force_sum_z*delta_t)
 
-
-
-		"""Periodic Boundary Conditions"""
-		# if self.pos.x < -1*L: 
-		# 	self.pos.x += 0
-		# 	#self.pos.y += L
-		# 	#self.pos.z += L
-		# if self.pos.y < -1*L:  
-		# 	#self.pos.x += L
-		# 	self.pos.y += 0
-		# 	#self.pos.z += L
-		# if self.pos.z < -1*L:  
-		# 	#self.pos.x += L
-		# 	#self.pos.y += L
-		# 	self.pos.z += 0
-
-		# if self.pos.x > L: 
-		# 	self.pos.x -= 0
-		# 	#self.pos.y -= L
-		# 	#self.pos.z -= L
-		# if self.pos.y > L:  
-		# 	#self.pos.x -= L
-		# 	self.pos.y -= 0
-		# 	#self.pos.z -= L
-		# if self.pos.z > L:  
-		# 	#self.pos.x -= L
-		# 	#self.pos.y -= L
-		# 	self.pos.z -= 0
 		""" Rotational Diffusion"""
 		# Variance is set to the rotational mean squared displacement
 		angle = np.random.normal(0,2*D_rot*delta_t,1) + (D_rot/kT)*self.radius*force_total*delta_t
@@ -131,33 +144,7 @@ class Fuse(compound):
 				#print("Distance: ", math.sqrt(distance_x**2 + distance_y**2 + distance_z**2))
 				# print(math.sqrt(distance_x**2 + distance_y**2 + distance_z**2) <0.5)
 		
-		""" Find out if dimerization should occur """
-		oligomer_pair_list = []
-		for j in range(0, len(distance_array_r)):
-			if distance_array_r[j] <= sigma and particle_num_array[j] != self.num: 
-				oligomer_pair_list.append((self.num,particle_num_array[j]))
-				self.hasOligomerized = True
-				break
-		
-		print(particle_array)
-		
-		for pair in oligomer_pair_list:
-			#print(pair)
-			temp_oligomer = Oligomer([particle_array[pair[0]],particle_array[pair[1]]])
-			
-			particle_array[pair[0]].visible = False
-			particle_array[pair[1]].visible = False
 
-			temp_oligomer.giveMass(particle_array[pair[0]], particle_array[pair[1]])
-			temp_oligomer.giveRadius(particle_array[pair[0]], particle_array[pair[1]])
-			temp_oligomer.giveNum(particle_array[pair[0]], particle_array[pair[1]])
-
-			particle_array.append(temp_oligomer)
-
-			
-		for pair in oligomer_pair_list:
-			del particle_array[pair[0]]
-			del particle_array[pair[1] - 1]
 
 		""" Potential Energy of Translation"""
 		force_sum_x = 0
@@ -178,13 +165,14 @@ class Fuse(compound):
 
 		return force_sum_x, force_sum_y, force_sum_z, force
 
+
 	def calculateDistance(self, particle2):
 		return abs(self.pos.x - particle2.pos.x), abs(self.pos.y - particle2.pos.y), abs(self.pos.z - particle2.pos.z), math.sqrt(((self.pos.x - particle2.pos.x)**2) +((self.pos.y - particle2.pos.y)**2) + (self.pos.z - particle2.pos.z)**2)
 
 class Oligomer(Fuse):
 
-	def giveNum(self,particle1,particle2): 
-		self.num = 2
+	def giveNum(self,particle_array_length): 
+		self.num = particle_array_length - 2
 
 	def giveMass(self, particle1, particle2):
 		self.mass = particle1.mass + particle2.mass
